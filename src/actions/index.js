94428ce374574c34373000eaec0bf61d187e7c5b
Eric Chen <ericchen0121@Erics-MacBook-Pro.local>
2017-08-15 18:06:17 -0700

    added styling on weather input form fields. bind action creator for fetchWeather to search bar container. Wire it up to the submission of the form input.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
+
 const API_KEY = '5ba3fb629a3c7c734440a0443f021b87';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = { term: '' };
 
     this.onInputChange = this.onInputChange.bind(this);
-
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -15,7 +18,9 @@ export default class SearchBar extends Component {
   }
 
   onFormSubmit(event) {
-    event.preventDefault(); // don't submit the form
+    event.preventDefault(); // don't submit the form, don't reload the page
+    this.props.fetchWeather(this.state.term); // make the API call to openWeathermap API
+    this.setState({ term: ''}) // reset the search term
   }
 
   render() {
@@ -24,12 +29,21 @@ export default class SearchBar extends Component {
         <input
           placeholder='Search for weather in your city'
           value={this.state.term}
+          className='form-control'
           onChange={this.onInputChange}
         />
-        <span>
-          <button type='submit'>Submit</button>
+        <span className='input-group-btn'>
+          <button type='submit' className='btn btn-secondary'>Submit</button>
         </span>
       </form>
     )
   }
 }
+
+// this.props.fetchWeather is now available
+//
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators( { fetchWeather }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
